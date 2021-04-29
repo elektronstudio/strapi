cf80874b86aa406ec9dc43e6bdbd20b163b09177
kristjanjansen <kristjan.jansen@gmail.com>
2021-04-29 10:02:27 +0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21b24f403612ec44d7886d6be95e8f00'),
+    },
+  },
+});
